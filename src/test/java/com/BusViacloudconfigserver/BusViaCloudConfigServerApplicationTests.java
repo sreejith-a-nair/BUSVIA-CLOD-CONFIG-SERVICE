66cabc75616da4e8b9698d65e460172d9fdc1cda
sreejith-a-nair <anairsreejith1998@gmail.com>
2024-01-17 14:22:32 +0530

    Initial commit

---
+++ src/test/java/com/BusViacloudconfigserver/BusViaCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BusViacloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusViaCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
